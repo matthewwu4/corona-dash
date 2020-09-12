439e3eb3def1124fddf7609d170c69f27e1fec06
mattwu <mattwu@umich.edu>
2020-09-13 01:54:24 +0800

    Updates

---
+++ precache-manifest.2dbe948779c111c28447177ee1c5b808.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dec0e9041580a2c4b7e8229492a53d50",
+    "url": "/corona-dash/index.html"
+  },
+  {
+    "revision": "9707b3cd89e242f1b5f6",
+    "url": "/corona-dash/static/css/main.13df3b11.chunk.css"
+  },
+  {
+    "revision": "c6e0a8515cf169d7a125",
+    "url": "/corona-dash/static/js/2.9003c83b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/corona-dash/static/js/2.9003c83b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9707b3cd89e242f1b5f6",
+    "url": "/corona-dash/static/js/main.06bb312b.chunk.js"
+  },
+  {
+    "revision": "ae432a687033bc35986f",
+    "url": "/corona-dash/static/js/runtime-main.efd3e94f.js"
+  }
+]);
